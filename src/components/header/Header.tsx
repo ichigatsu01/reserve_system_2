4d7ea1a5b8dc34c79177c41f3c04f44114306ede
ichigatsu <firstmoon1986@gmail.com>
2025-05-30 06:58:55 +0900

    ログイン画面のエラーチェック実装

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,29 +1,35 @@
-import { Box, Stack } from '@mui/material'
-import './Header.css'
+import { Box, Button, Stack, Tooltip } from '@mui/material'
+// import './Header.css'
 import MenuIcon from '@mui/icons-material/Menu';
 import logo from '../../assets/logo.png';
 
 const Header = () => {
     return (
         <>
-            <Stack sx={{display:{xs: 'none', sm: 'block'}}}>
-                <header id="header">
-                    <Box sx={{ width: 150, height: 'auto'  }}>
-                        <img src={logo} alt="ささみまぐろペットクリニック" style={{ width: '100%', height: 'auto' }} />
-                    </Box>
-                    <div id="navi">
-                        <ul>
-                            <li>HOME</li>
-                            <li>診療について</li>
-                            <li>よくある質問</li>
-                            <li>医師の紹介</li>
-                            <li>受診の仕方</li>
-                            <li id="reserve">Web順番予約</li>
-                            <li>アクセス</li>
-                            <li>お知らせ</li>
-                        </ul>
-                    </div>
-                </header>
+            <Stack spacing={2} direction={'row'} sx={{
+                mx:0, my:0, px:'30px', py:'10px', bgcolor:"#79A1D4", textAlign:'center', alignItems:'center',
+                display:{xs: 'none', sm: 'flex'}, justifyContent:'space-between'
+                }}>
+                <Box sx={{ width: 150, height: 'auto'  }}>
+                    <img src={logo} alt="ささみまぐろペットクリニック" style={{ width: '100%', height: 'auto' }} />
+                </Box>
+                <Stack direction={'row'} spacing={2}>
+                    <Tooltip title="HOME" arrow>
+                        <Button sx={{color: "white"}}>HOME</Button>
+                    </Tooltip>
+                    <Tooltip title="院長からの挨拶" arrow>
+                        <Button sx={{color: "white"}}>院長からの挨拶</Button>
+                    </Tooltip>
+                    <Tooltip title="診療時間" arrow>
+                        <Button sx={{color: "white"}}>診療時間</Button>
+                    </Tooltip>
+                    <Tooltip title="Web順番予約" arrow>
+                        <Button sx={{color: "#454545", backgroundColor: '#eee', borderRadius: '15px'}}>Web順番予約</Button>
+                    </Tooltip>
+                    <Tooltip title="アクセス" arrow>
+                        <Button sx={{color: "white"}}>アクセス</Button>
+                    </Tooltip>
+                </Stack>
             </Stack>
             <Stack spacing={2} direction={'row'} sx={{
                 mx:0, my:0, padding:'10px', bgcolor:"#79A1D4", textAlign:'center', alignItems:'center',
@@ -33,7 +39,7 @@ const Header = () => {
                     <img src={logo} alt="ささみまぐろペットクリニック" style={{ width: '100%', height: 'auto' }} />
                 </Box>
                 <Box sx={{marginRight: '20px'}}>
-                    <MenuIcon fontSize='large' />    
+                    <MenuIcon fontSize='large' />
                 </Box>
             </Stack>
         </>
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,18 +1,53 @@
 // import React from 'react'
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { FunctionContext } from "../providers/FunctionProvider"
-import { Button, Divider, Stack, TextField, Typography } from "@mui/material"
+import { Alert, Button, Divider, Stack, TextField, Typography } from "@mui/material"
 
 
 const Login = () => {
     // Contextからの輸入
     const { setIsLogin, patientNum, setPatientNum, patientName ,setPatientName, patientTel, setPatientTel } = useContext(FunctionContext)
 
+    // 入力エリアに対する警告
+    const [ alertMessage, setAlertMessage ] = useState<string[]>([])
+    const [ isNumError, setIsNumError ] = useState(false)
+    const [ isNameError, setIsNameError ] = useState(false)
+    const [ isTelError, setIsTelError ] = useState(false)
+    const isOnlyDigits = (text: string) => /^[0-9]+$/.test(text);
     const handleLogin = () => {
+        // 診療番号チェック
         console.log(patientNum)
+        let newAlert: string[] = [];
+        if (patientNum.trim() == "") {
+            newAlert.push("診療番号が入力されていません")
+            setIsNumError(true)
+        } else if (!isOnlyDigits(patientNum)) {
+            newAlert.push("診療番号には数字のみ入力してください")
+            setIsNumError(true)
+        } else {
+            setIsNumError(false)
+        }
+        // 飼い主名チェック
         console.log(patientName)
+        if (patientName.trim() == "") {
+            newAlert.push("飼い主様のお名前が入力されていません")
+            setIsNameError(true)
+        } else {
+            setIsNameError(false)
+        }
+        // 電話番号チェック
         console.log(patientTel)
-    }
+        if (patientTel.trim() == "") {
+            newAlert.push("電話番号が入力されていません")
+            setIsTelError(true)
+        } else if (!isOnlyDigits(patientTel)) {
+            newAlert.push("電話番号には数字のみ入力してください")
+            setIsTelError(true)
+        } else {
+            setIsTelError(false)
+        }
+        setAlertMessage(newAlert)
+    };
 
     return (
         <Stack spacing={4} sx={{maxWidth: '400px', mx: 'auto', my: '25px'}}>
@@ -25,16 +60,27 @@ const Login = () => {
             <Divider variant="middle" sx={{borderColor: '#454545'}}/>
             <Stack spacing={2}>
                 <Typography variant="h5">入力フォーム</Typography>
-                <TextField label="診療番号" required onChange={(e) =>setPatientNum(e.target.value)}/>
-                <TextField label="飼い主様のお名前" required onChange={(e) =>setPatientName(e.target.value)}/>
-                <TextField label="電話番号下4桁" required onChange={(e) =>setPatientTel(e.target.value)}/>
+                {alertMessage.length > 0 && //alertMessageに何らかのメッセージが入っているか？
+                        alertMessage.map((msg, index) => (
+                            <Alert key={index} severity="warning" variant="filled">{msg}</Alert>
+                        ))
+                }
+                <TextField label="診察番号" required error={isNumError}
+                    onChange={(e) =>setPatientNum(e.target.value)}
+                />
+                <TextField label="飼い主様のお名前" required error={isNameError}
+                    onChange={(e) =>setPatientName(e.target.value)}
+                />
+                <TextField label="電話番号下4桁" required error={isTelError}
+                    onChange={(e) =>setPatientTel(e.target.value)}
+                />
                 <Button
                     variant="contained"
-                    // onClick={() => setIsLogin(true)}
                     onClick={handleLogin}
                 >
                     予約画面へ
                 </Button>
+            
             </Stack>
         </Stack>
     )
